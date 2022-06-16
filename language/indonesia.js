1c22258414c414c655eb4b655de249c8f60b7c18
RullMoon <107094781+RullMoon@users.noreply.github.com>
2022-06-16 10:10:44 +0700

    Update indonesia.js

--- language/indonesia.js
+++ language/indonesia.js
@@ -842,7 +842,13 @@ Arigatou Gozaimasu!
 `
 }
 exports.welcome = () =>{
-	return`Welcome Member Baru, Patuhi Rules Grub Maupun Bot Ya
+	return`𝖩𝖺𝗇𝗀𝖺𝗇 𝗅𝗎𝗉𝖺 𝗂𝗇𝗍𝗋𝗈 𝗄𝖺𝗄 🤗
+	
+╭ 𝖭𝖺𝗆𝖺 :
+├ 𝖴𝗆𝗎𝗋 :
+├ 𝖦𝖾𝗇𝖽𝖾𝗋 :
+├ 𝖠𝗌𝗄𝗈𝗍 :
+╰❒ 𝖯𝖺𝗍𝗎𝗁𝗂 𝗋𝗎𝗅𝖾𝗌 𝗀𝗋𝗎𝗉 𝗒𝖺 𝗄𝖺k ~`
 }
 exports.leave = () =>{
 	return`Yah kok out 😣 Mental aman kan kak >.<
